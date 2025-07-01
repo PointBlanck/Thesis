355f0116eae7c24f7b06fdeb05814e1e93e69526
TeoKav <kavalagiost@gmail.com>
2025-07-01 12:10:14 +0300

    Code Progress

--- python/potentials.py
+++ python/potentials.py
@@ -11,8 +11,25 @@ M_d = 8.56 * 10**(10) # M_sol
 a_d = 5.3 # kpc
 b_d = 0.25 # kpc
 # Plummer Bulge potential constants
-M_b = 5 * 10**(10) # M_sol
-b = 1.5 # kpc
+M_b = 5.0 * 10**(10) # M_sol
+beta = 1.5 # kpc
+# Gamma Halo potential constants
+M_h0 = 10.7 * 10**(10) # M_sol
+gamma = 1.02
+r_hmax = 100 # kpc
+r_h = 12.0 # kpc
+# Logarithmic Spiral potential constants
+h_z = 0.18 # kpc
+rho_0 = 5.0 * 10**7 # M_sol/(kpc^(-3))
+C = 8.0/(3.0*np.pi)
+consts = 4.0*np.pi*G*h_z*rho_0*C
+pitch_angle = -13.0 # degrees
+alpha = (pitch_angle*np.pi)/180.0 # rads
+b = 0.474
+c = 0.335
+R_s0 = 6.0 # kpc
+r_0 = 8.0 # kpc
+R_s = 7.0 # kpc
 
 def disk_potential(r):
     """ 
@@ -26,3 +43,24 @@ def bulge_potential(r):
     Plummer Model for the bulge gravitational potential component.
     """
     return (-G*M_b)/np.sqrt(r**2 + b**2)
+
+#??????????????????????????????????????????????????????
+# Need to check the formula of the halo potential.
+# The paper most likely contains a mistake.
+#??????????????????????????????????????????????????????
+def halo_potential(r):
+    """
+    Gamma Model for the halo gravitational potential component.
+    """
+    r_ratio = r/r_h
+    M_hr = (M_h0*((r_ratio)**(gamma+1.0)))/(1 + r_ratio**gamma)
+    return ((-G*M_hr)/r) + ((G*M_h0)/(gamma*r))*((-gamma/(1+(r_ratio**gamma))) + (np.log(1 + r_ratio)**gamma))
+
+def spiral_potential(r, phi):
+    """
+    Logarithmic Model for the spiral gravitational perturbation potential.
+    """
+    K = 2/(r*np.abs(np.sin(alpha)))
+    khz = K*h_z
+    B = (1.0 + khz + 0.3*(khz)**2)/(1 + 0.3*khz)
+    return consts*(b - c*np.atan(R_s0 - r))*np.exp(-(r - r_0)/R_s)*np.cos(2*(phi - (np.log(r/r_0)/np.tan(alpha))))
--- python/test.py
+++ python/test.py
@@ -4,16 +4,49 @@ Module containing testing code for main program.
 
 # Importing necessary modules
 import potentials as ptns
+import numpy as np
+import matplotlib.pyplot as plt
 
 #=====================================================
-# Potential Tests
+# Tests
 #=====================================================
 def test_potentials():
     """
     Function performing tests on the potential functions.
     """
-    print(ptns.disk_potential(0))
-    print(ptns.bulge_potential(0))
+    print(ptns.disk_potential(6))
+    print(ptns.bulge_potential(6))
+    print(ptns.halo_potential(6))
+    print(ptns.spiral_potential(6,0.6))
+    #=======================================================
+    # Potential plots
+    #=======================================================
+    
+    # Prepare the potentials for plotting.
+
+    r = np.linspace(1, 100, 10000)
+    disk = ptns.disk_potential(r)
+    bulge = ptns.bulge_potential(r)
+    halo = ptns.halo_potential(r)
+    total = disk + bulge + halo
+    disk_norm = disk/total
+    bulge_norm = bulge/total
+    halo_norm = halo/total
+
+    # Make the plots
+    fig, ax = plt.subplots(1,2)
+    ax[0].plot(r, disk, label="Disk")
+    ax[0].plot(r, bulge, label="Bulge")
+    ax[0].plot(r[1200:], halo[1200:], label="Halo")
+    ax[0].grid(True)
+    ax[0].legend()
+    ax[1].plot(r, bulge_norm, label="Bulge")
+    ax[1].plot(r, disk_norm, label="Disk")
+    ax[1].plot(r, halo_norm, label="Halo")
+    ax[1].set_xscale("log")
+    ax[1].grid("True")
+    ax[1].legend()
+    plt.show()
 
 test_potentials()
 
