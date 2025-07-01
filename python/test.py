41af545dbd89444b21bdda40c02a4be9a97141ff
TeoKav <kavalagiost@gmail.com>
2025-07-01 13:11:11 +0300

    Code Progress

--- python/test.py
+++ python/test.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 #=====================================================
 # Tests
 #=====================================================
-def test_potentials():
+def plot_axisymmetric_potentials(r):
     """
     Function performing tests on the potential functions.
     """
@@ -23,15 +23,18 @@ def test_potentials():
     #=======================================================
     
     # Prepare the potentials for plotting.
-
-    r = np.linspace(1, 100, 10000)
+    # Unless the problem with the halo potential is fixed
+    # we compartmentalized the plotting of our function.
     disk = ptns.disk_potential(r)
     bulge = ptns.bulge_potential(r)
     halo = ptns.halo_potential(r)
-    total = disk + bulge + halo
-    disk_norm = disk/total
-    bulge_norm = bulge/total
-    halo_norm = halo/total
+    total1 = disk[0:1200] + bulge[0:1200]
+    total2 = disk[1200:] + bulge[1200:] + halo[1200:]
+    disk_norm1 = disk[0:1200]/total1
+    disk_norm2 = disk[1200:]/total2
+    bulge_norm1 = bulge[0:1200]/total1
+    bulge_norm2 = bulge[1200:]/total2
+    halo_norm = halo[1200:]/total2
 
     # Make the plots
     fig, ax = plt.subplots(1,2)
@@ -40,13 +43,22 @@ def test_potentials():
     ax[0].plot(r[1200:], halo[1200:], label="Halo")
     ax[0].grid(True)
     ax[0].legend()
-    ax[1].plot(r, bulge_norm, label="Bulge")
-    ax[1].plot(r, disk_norm, label="Disk")
-    ax[1].plot(r, halo_norm, label="Halo")
+    ax[1].plot(r[0:1200], bulge_norm1, label="Bulge 1")
+    ax[1].plot(r[1200:], bulge_norm2, label="Bulge 2")
+    ax[1].plot(r[0:1200], disk_norm1, label="Disk 1")
+    ax[1].plot(r[1200:], disk_norm2, label="Disk 2")
+    ax[1].plot(r[1200:], halo_norm, label="Halo 1")
     ax[1].set_xscale("log")
     ax[1].grid("True")
     ax[1].legend()
     plt.show()
 
-test_potentials()
+def f_strength():
+    """
+    This function plots the F-Strength of the potentials.
+    """
+    pass
+
+r = np.linspace(1, 100, 10000)
+plot_axisymmetric_potentials(r)
 
