737092eccff95e37fd23f1d66924f471466e57ac
TeoKav <kavalagiost@gmail.com>
2025-07-10 01:41:22 +0300

    Integration module initialization

--- python/integration_sym.py
+++ python/integration_sym.py
@@ -2,3 +2,6 @@
 Module containing code used for integrating the dynamical galactic system.
 Specifically designed to maintain certain quaulitative functions.
 """
+
+# Import necessary modules
+import scipy as scp
